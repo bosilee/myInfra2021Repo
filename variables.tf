9591b53f5cf7ae5a80f0c1fab7563f9da950dd8e
Bosi Lee <65838247+bosilee@users.noreply.github.com>
2023-05-10 02:45:10 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "westrock" 
 }
 
 variable "instance_type" { 
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "westrock" 
+    default     = "jenkins-sgroup-dec-2021" 
 }
 
 variable "tag_name" { 
